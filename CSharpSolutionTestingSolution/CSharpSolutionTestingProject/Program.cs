32c22970fc33f6fd50ee673d5b6a6f82ada36ee8
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-02-25 01:45:45 -0500

    Create testing program with initial brute force twosum solution

---
+++ CSharpSolutionTestingSolution/CSharpSolutionTestingProject/Program.cs
@@ -0,0 +1,47 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Solution/Project:   CSharpSolutionTestingProjectSolution/CSharpSolutionTestingProject
+//  File Name:          Driver.cs
+//  Description:        Main driver for program
+//  Authors:            Bobby Mullins
+//  Created:            Friday, February 10, 2023 | (2023-02-10)
+//  Copyright:          N/A
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace CSharpSolutionTestingProject
+{
+    /// <summary>
+    ///  Driver class for starting program
+    /// </summary>
+    internal static class Driver
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            TestTwoSum();
+        }
+
+        //Method for testing TwoSum solution
+        public static void TestTwoSum()
+        {
+            //Main variables
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] returnArray = TwoSumStatic.TwoSumBruteForce(nums, target); //Brute force method
+            //More efficient method
+
+
+            //Solution printed out to console
+            returnArray.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+        }
+
+    }//end Driver class
+
+}//end CSharpSolutionTestingProject namespace
---
+++ CSharpSolutionTestingSolution/CSharpSolutionTestingProject/TwoSumStatic.cs
@@ -0,0 +1,54 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////
+//
+//  Solution/Project:   CSharpSolutionTestingProjectSolution/CSharpSolutionTestingProject
+//  File Name:          TwoSumStatic.cs
+//  Description:        TwoSumStatic class for testing solutions to the TwoSum problem
+//  Authors:            Bobby Mullins
+//  Created:            Friday, February 10, 2023 | (2023-02-10)
+//  Copyright:          N/A
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutionTestingProject
+{
+    /// <summary>
+    ///  Class for testing static methods and functions
+    /// </summary>
+    internal class TwoSumStatic
+    {
+        //Brute force solution for TwoSum problem
+        public static int[] TwoSumBruteForce(int[] nums, int target)
+        {
+            int i, j; //loop variables
+
+            for (i = 0; i < nums.Length; i++)
+            {
+                for (j = 0; j < nums.Length; j++)
+                {
+                    if (i != j) //do not compare if same index
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[] { i, j };
+                        }
+                    }
+                }
+            }
+
+            return new int[] { 0, 0 };
+        }
+
+        //More efficient solution for TwoSum problem (might not be the best)
+        public static int[] TwoSumEfficient(int[] nums, int target)
+        {
+            return new int[] { 0, 0 };
+        }
+
+        }//end StaticTesting
+}
