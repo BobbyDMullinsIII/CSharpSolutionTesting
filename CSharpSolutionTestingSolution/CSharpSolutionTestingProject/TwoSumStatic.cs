b8c6d3a5414aed5d4afa45221afc489914a1e65c
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-02-25 23:01:06 -0500

    Added more efficient dictionary solution that only passes nums one time

--- CSharpSolutionTestingSolution/CSharpSolutionTestingProject/TwoSumStatic.cs
+++ CSharpSolutionTestingSolution/CSharpSolutionTestingProject/TwoSumStatic.cs
@@ -32,21 +32,27 @@ namespace CSharpSolutionTestingProject
         {
             int i, j; //loop variables
 
+            //double for loop to iterate through each value and compare to every other value
+            //(will result in comparisons with the same indices duclicating a few times)
             for (i = 0; i < nums.Length; i++)
             {
                 for (j = 0; j < nums.Length; j++)
                 {
                     if (i != j) //do not compare if same index
                     {
+                        //Check if equal to target
                         if (nums[i] + nums[j] == target)
                         {
+                            //returns indices if two separate indices to equal target are found
                             return new int[] { i, j };
                         }
                     }
                 }
             }
 
+            //Will only return this if two separate indices equaling target are not found
             return new int[] { 0, 0 };
+
         }//end TwoSumBruteForce()
 
         /// <summary>
@@ -57,9 +63,28 @@ namespace CSharpSolutionTestingProject
         /// <returns>array of indices within nums array containing integer sum equal to target</returns>
         public static int[] TwoSumEfficient(int[] nums, int target)
         {
+            int i; //loop variable
+            Dictionary<int, int> dict = new Dictionary<int, int>(); //dictionary (hashmap equivalent)
 
+            //loop through array only one time using dictionary
+            for (i = 0; i < nums.Length; i++)
+            {
+                int indiceOne = nums[i];
+                int indiceTwo = target - indiceOne;
 
+                //Check if equal to target
+                if (dict.TryGetValue(indiceTwo, out int index))
+                {
+                    //returns indices if two separate indices to equal target are found
+                    return new[] { index, i };
+                }
+
+                dict[indiceOne] = i;
+            }
+
+            //Will only return this if two separate indices equaling target are not found
             return new int[] { 0, 0 };
+
         }//end TwoSumEfficient()
 
     }//end StaticTesting
